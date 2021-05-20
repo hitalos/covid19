e51fddb725b4f0a00d8604ff69ec30661f477492
Hítalo Silva <hitalos@gmail.com>
2021-05-20 02:37:14 -0300

    Improving filter on CVS download script

--- updateCases.sh
+++ updateCases.sh
@@ -12,16 +12,18 @@ if [[ "$2" != "" ]]; then
 	STATES=$2
 fi
 
-REPO=$DIR/dados
+lastMonths=$(date --date="-1 month" +%Y-%m\|)$(date +%Y-%m)
 
+REPO=$DIR/dados
 curl -sS 'https://data.brasil.io/dataset/covid19/caso.csv.gz' | gzip -d > "$TEMP"
-awk 'BEGIN { FS=","; OFS="," } /state/ { print $1,$2,$5,$6,$8,$10,$11 }' "$TEMP" > "$REPO/casos.csv"
+echo "date,state,confirmed,deaths,is_last,estimated_population,city_ibge_code" > "$REPO/casos.csv"
+awk "BEGIN { FS=\",\"; OFS=\",\" } /($lastMonths).*state/ { print \$1,\$2,\$5,\$6,\$8,\$10,\$11 }" "$TEMP" >> "$REPO/casos.csv"
 gzip -9 -k -f "$REPO/casos.csv"
 
 for UF in $STATES; do
 	echo "Filtrando dados de $UF..."
 	echo "date,state,city,confirmed,deaths,is_last" > "$REPO/$UF/casos.csv"
-	awk "BEGIN { FS=\",\"; OFS=\",\" } /,$UF,.*,city/ { print \$1,\$2,\$3,\$5,\$6,\$8 }" "$TEMP" >> "$REPO/$UF/casos.csv"
+	awk "BEGIN { FS=\",\"; OFS=\",\" } /($lastMonths).*,$UF,.*,city/ { print \$1,\$2,\$3,\$5,\$6,\$8 }" "$TEMP" >> "$REPO/$UF/casos.csv"
 	gzip -9 -k -f "$REPO/$UF/casos.csv"
 done
 
